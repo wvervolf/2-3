8d6468adf59eb0cd21c8797a88db0a5e5c2cb985
wvervolf <wvervolf@gmail.com>
2024-04-06 23:09:11 +0300

    initiall commit 2

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
